09df5ba874afd8d173d01a728ed97c221ddba03b
José Valim <jose.valim@plataformatec.com.br>
2018-03-13 10:52:05 +0100

    Add simple language example with string interpolation

---
+++ examples/simple_language.exs
@@ -0,0 +1,36 @@
+# Run it from root with `mix run examples/simple_language.exs`
+
+# A language with integers and interpolated strings.
+defmodule SimpleLanguage do
+  import NimbleParsec
+
+  interpolation =
+    ignore(string(~S(#{)))
+    |> parsec(:language)
+    |> ignore(string(~S(})))
+    |> unwrap_and_tag(:interpolation)
+
+  integer =
+    integer(min: 1)
+    |> unwrap_and_tag(:integer)
+
+  string =
+    ascii_char([?"])
+    |> repeat_until(
+      choice([
+        ~S(\") |> string() |> replace(?"),
+        ~S(\#) |> string() |> replace(?#),
+        interpolation,
+        utf8_char([])
+      ]),
+      [ascii_char([?"])]
+    )
+    |> ascii_char([?"])
+    |> tag(:string)
+
+  defparsec :language, choice([string, integer])
+end
+
+IO.inspect SimpleLanguage.language(~S(123))
+IO.inspect SimpleLanguage.language(~S("string with quotes \" inside"))
+IO.inspect SimpleLanguage.language(~S("string with #{123} interpolation inside"))
